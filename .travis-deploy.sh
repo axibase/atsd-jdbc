58823f99514f2bffa7e450b2b9dffa44eaf57511
Anton Rybochkin <anton.rybochkin@axibase.com>
2019-08-13 15:32:38 +0300

    Update .travis-deploy.sh

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [[ $JAVA_HOME = *"java-7-openjdk"* && $TRAVIS_PULL_REQUEST != "false" ]]; then
+if [[ $JAVA_HOME == *"java-7-openjdk"* && $TRAVIS_PULL_REQUEST != "false" ]]; then
     echo "SNAPSHOT DEPLOYING"
     mvn -Ptravis-deploy-snapshot deploy --settings ./.travis.settings.xml -B -Dgpg.skip
 else
